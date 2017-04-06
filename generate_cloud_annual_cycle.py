5524d586a717a2e8ca8028b0dbe7eca68d00b4e8
Kate Marvel <kdm2144@columbia.edu>
2017-04-06 13:20:06 -0400

    changed try-except loop

--- generate_cloud_annual_cycle.py
+++ generate_cloud_annual_cycle.py
@@ -38,76 +38,78 @@ def write_amplitude_phase(experiment,variable,search_string = "*",overwrite=Fals
     files = cmip5.only_most_recent(glob.glob(path+search_string))
     bad = open("failed_files.dat","w")
     for fname in files:
-        f = cdms.open(fname)
-        X = f(variable)
-        #Need to ensure that X contains full annual cycle (HADLEY CENTER MODELS START IN DECEMBER AND ARE THE WORST BOOOOO HADLEY CENTER)
-        if cmip5.start_time(X).month != 1: #if it doesn't start in January, make it start in January
-            starttime = cdtime.comptime(cmip5.start_time(X).year+1,1,1)
-        else:
-            starttime = cmip5.start_time(X)
-        if cmip5.stop_time(X).month != 12: #if it doesn't stop in December, make it.
-            stoptime = cdtime.comptime(cmip5.stop_time(X).year-1,12,31)
-        else:
-            stoptime = cmip5.stop_time(X)
-        X = X(time=(starttime,stoptime))
-            
-        fshort = fname.split("/")[-1]
-        fwrite = fshort.replace("xml","nc")
-        fwrite = fwrite.replace(variable,variable+"AmpPhase")
-        if overwrite is False:
-            if fwrite in os.listdir(writepath):
-                continue
-        writefile = cdms.open(writepath+fwrite,"w")
         try:
-            R,P = sc.fast_annual_cycle(X)
+            f = cdms.open(fname)
+            X = f(variable)
+            #Need to ensure that X contains full annual cycle (HADLEY CENTER MODELS START IN DECEMBER AND ARE THE WORST BOOOOO HADLEY CENTER)
+            if cmip5.start_time(X).month != 1: #if it doesn't start in January, make it start in January
+                starttime = cdtime.comptime(cmip5.start_time(X).year+1,1,1)
+            else:
+                starttime = cmip5.start_time(X)
+            if cmip5.stop_time(X).month != 12: #if it doesn't stop in December, make it.
+                stoptime = cdtime.comptime(cmip5.stop_time(X).year-1,12,31)
+            else:
+                stoptime = cmip5.stop_time(X)
+            X = X(time=(starttime,stoptime))
+
+            fshort = fname.split("/")[-1]
+            fwrite = fshort.replace("xml","nc")
+            fwrite = fwrite.replace(variable,variable+"AmpPhase")
+            if overwrite is False:
+                if fwrite in os.listdir(writepath):
+                    continue
+            writefile = cdms.open(writepath+fwrite,"w")
+
+                R,P = sc.fast_annual_cycle(X)
+
+            historical = experiment.find("historical")>=0
+            if historical:
+                relative_to = "1996-2009"
+            else:
+                relative_to = str(cmip5.start_time(X).year)+"-"+str(cmip5.stop_time(X).year)
+            Pa = sc.get_phase_anomalies(P,historical=historical)
+            if historical:
+                Rclim = MV.average(R(time=('1996-1-1','2009-12-31')),axis=0) #this anomaly period chosen to overlap ISCCP/PATMOS most recent time period with all data present
+            else:
+                Rclim = MV.average(R,axis=0)
+            Ra = R-Rclim
+
+
+            R.id = "amp"
+            if hasattr(X,"umits"):
+                R.units = X.units
+            R.long_name = "Amplitude of the "+variable+" annual cycle"
+            writefile.write(R)
+
+            P.id = "phase"
+            P.units = "radians"
+            P.long_name = "Phase of the "+variable+" annual cycle"
+            writefile.write(P)
+
+            Ra.id = "amp_anom"
+            if hasattr(X,"umits"):
+                Ra.units = X.units
+            Ra.long_name = "Amplitude anomaly relative to "+relative_to
+            writefile.write(Ra)
+
+            Pa.id = "phase_anom"
+            Pa.units = "days"
+            Pa.long_name = "Phase anomaly relative to "+relative_to
+            writefile.write(Pa)
+
+            f.close()
+            writefile.close()
         except:
             bad.write(fname+"\n")
             
             continue
-        historical = experiment.find("historical")>=0
-        if historical:
-            relative_to = "1996-2009"
-        else:
-            relative_to = str(cmip5.start_time(X).year)+"-"+str(cmip5.stop_time(X).year)
-        Pa = sc.get_phase_anomalies(P,historical=historical)
-        if historical:
-            Rclim = MV.average(R(time=('1996-1-1','2009-12-31')),axis=0) #this anomaly period chosen to overlap ISCCP/PATMOS most recent time period with all data present
-        else:
-            Rclim = MV.average(R,axis=0)
-        Ra = R-Rclim
-        
-
-        R.id = "amp"
-        if hasattr(X,"umits"):
-            R.units = X.units
-        R.long_name = "Amplitude of the "+variable+" annual cycle"
-        writefile.write(R)
-
-        P.id = "phase"
-        P.units = "radians"
-        P.long_name = "Phase of the "+variable+" annual cycle"
-        writefile.write(P)
-        
-        Ra.id = "amp_anom"
-        if hasattr(X,"umits"):
-            Ra.units = X.units
-        Ra.long_name = "Amplitude anomaly relative to "+relative_to
-        writefile.write(Ra)
-        
-        Pa.id = "phase_anom"
-        Pa.units = "days"
-        Pa.long_name = "Phase anomaly relative to "+relative_to
-        writefile.write(Pa)
-        
-        f.close()
-        writefile.close()
     bad.close()
     
 
     
     
 if __name__ == "__main__":
-    experiments = ["historical","rcp85","1pctCO2","piControl"]
+    experiments = ["piControl","rcp85","1pctCO2"]
     variable = "clt"
     for experiment in experiments:
         
