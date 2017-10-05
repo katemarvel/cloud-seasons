8cc7339a266eaa93b619570101d954ff3bc23f58
Kate Marvel <kdm2144@columbia.edu>
2017-10-05 17:40:02 -0400

    Added low/high cloud averager for historical-rcp85 and nat

--- clisccp.py
+++ clisccp.py
@@ -50,6 +50,10 @@ def low_cloud(clisccp,thresh=440,nyears=200):
     fobs.close()
     return annual_cycle_removed_regrid
 
+
+
+
+
 def high_cloud(clisccp,thresh=440,nyears=200):
  
     axes = clisccp.getAxisIds()
@@ -101,3 +105,85 @@ def write_piControl_cloud():
     fl.write(LOW)
     fl.close()
     return HIGH,LOW
+
+
+
+
+### Historical
+def low_cloud_historical(clisccp,thresh=440,start='1983-7-1',stop='2005-12-31'):
+ 
+    axes = clisccp.getAxisIds()
+
+ 
+ 
+    plev_ax = clisccp.getAxisIds().index("plev")
+    low = MV.sum(clisccp(level=(1000*100,thresh*100)),axis=plev_ax)(time=(start,stop))
+    cdutil.setTimeBoundsMonthly(low)
+    annual_cycle_removed= cdutil.ANNUALCYCLE.departures(low)
+ 
+    if crunchy:
+        fobs = cdms.open("/work/marvel1/CLOUD_SEASONS/cloud-seasons/CLOUD_OBS/clt_ISCCP_corrected_198301-200912.nc")
+    else:
+        fobs = cdms.open("/Users/kmarvel/Google Drive/CLOUD_SEASONS/cloud-seasons/CLOUD_OBS/clt_ISCCP_corrected_198301-200912.nc")
+    the_grid = fobs["clt"].getGrid()
+    annual_cycle_removed_regrid = annual_cycle_removed.regrid(the_grid,regridTool='regrid2')
+    fobs.close()
+    return annual_cycle_removed_regrid
+
+def high_cloud_historical(clisccp,thresh=440,start='1983-7-1',stop='2005-12-31'):
+ 
+    axes = clisccp.getAxisIds()
+
+ 
+ 
+    plev_ax = clisccp.getAxisIds().index("plev")
+    high = MV.sum(clisccp(level=(thresh*100,0)),axis=plev_ax)(time=(start,stop))
+    cdutil.setTimeBoundsMonthly(high)
+    annual_cycle_removed= cdutil.ANNUALCYCLE.departures(high)
+ 
+    if crunchy:
+        fobs = cdms.open("/work/marvel1/CLOUD_SEASONS/cloud-seasons/CLOUD_OBS/clt_ISCCP_corrected_198301-200912.nc")
+    else:
+        fobs = cdms.open("/Users/kmarvel/Google Drive/CLOUD_SEASONS/cloud-seasons/CLOUD_OBS/clt_ISCCP_corrected_198301-200912.nc")
+    the_grid = fobs["clt"].getGrid()
+    annual_cycle_removed_regrid = annual_cycle_removed.regrid(the_grid,regridTool='regrid2')
+    fobs.close()
+    return annual_cycle_removed_regrid
+
+def write_historical_cloud():
+    direc = "/kate/cl_regrid_isccp/historical-rcp85/XML/"
+    search_string = "*r1*xml"
+    HIGH = cmip5.get_ensemble(direc,"pgrid_cl",search_string=search_string,func=high_cloud,stop='2099-12-31')
+    HIGH.id="high_clt"
+    fh = cdms.open("cmip5.historical-rcp85.ensemble.high_clt.nc","w")
+    fh.write(HIGH)
+    fh.close()
+    
+    LOW = cmip5.get_ensemble(direc,"pgrid_cl",search_string=search_string,func=low_cloud,stop='2099-12-31')
+    LOW.id="low_mid_clt"
+    fl = cdms.open("cmip5.historical-rcp85.ensemble.lowmid_clt.nc","w")
+    fl.write(LOW)
+    fl.close()
+   # return HIGH,LOW
+
+def write_Nat_cloud():
+    direc = "/kate/cl_regrid_isccp/historicalNat/XML/"
+    search_string = "*r1*xml"
+    HIGH = cmip5.get_ensemble(direc,"pgrid_cl",search_string=search_string,func=high_cloud,stop='2099-12-31')
+    HIGH.id="high_clt"
+    fh = cdms.open("cmip5.historicalNat.ensemble.high_clt.nc","w")
+    fh.write(HIGH)
+    fh.close()
+    
+    LOW = cmip5.get_ensemble(direc,"pgrid_cl",search_string=search_string,func=low_cloud,stop='2099-12-31')
+    LOW.id="low_mid_clt"
+    fl = cdms.open("cmip5.historicalNat.ensemble.lowmid_clt.nc","w")
+    fl.write(LOW)
+    fl.close()
+    #return HIGH,LOW
+
+if __name__ == "__main__":
+    HIGH_piC,LOW_piC= write_piControl_cloud()
+    write_Nat_cloud()
+    write_historical_cloud()
+    
